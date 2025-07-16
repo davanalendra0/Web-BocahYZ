f76d56ac25d32403c35ca8ca42c77ddc3aefa223
Dava Nalendra <davanalendra2@gmail.com>
2025-07-16 11:26:31 +0700

    Update galeri

--- js/script.js
+++ js/script.js
@@ -8,3 +8,21 @@ menuBtn.addEventListener('click', () => {
 navMenu.querySelectorAll('a').forEach(link =>
   link.addEventListener('click', () => navMenu.classList.remove('active'))
 );
+
+new Swiper('.gallery-slider', {
+  loop: true,
+  autoplay: {
+    delay: 2300,
+    disableOnInteraction: false,
+  },
+  slidesPerView: 1,
+  spaceBetween: 30,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    }
+  }
+});
